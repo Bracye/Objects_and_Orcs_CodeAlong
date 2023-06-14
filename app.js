f200fc7ac5e080bef4197f6e110ed5bffd701ecb
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-14 11:23:23 -0400

    created an object

---
+++ app.js
@@ -0,0 +1,9 @@
+//Array inside an obj
+
+const adventurer = {
+  name: "Ernest",
+  hitPoint: 10,
+  belongings: ["sword", "potions", "medicine"],
+};
+//Access values in the belongings array
+console.log(adventurer.belongings);
