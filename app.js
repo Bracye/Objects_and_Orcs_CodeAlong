272bd23977221c1059cbcd1aee5107f6108712b1
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-14 11:34:24 -0400

    objects within objects

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const adventurer = {
   name: "Ernest",
   hitPoint: 10,
   belongings: ["sword", "potions", "medicine"],
+  companion: {
+    name: "K.C.",
+    type: "Dog",
+  },
 };
-//Access values in the belongings array
-console.log(adventurer.belongings);
+
+console.log(adventurer.companion.name);
+// //Access values in the belongings array
+// console.log(adventurer.belongings);
+
+// //Access the first belonging
+// console.log(` first belonging: ${adventurer.belongings[0]}`);
+
+// //iterate over an array inside of an object
+
+// for (let i = 0; i < adventurer.belongings.length; i++) {
+//   console.log(adventurer.belongings[i]);
+// }
+
+// #1 Obj within Obj
