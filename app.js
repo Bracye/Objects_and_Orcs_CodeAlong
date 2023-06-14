33991051d25d2a163b67958c8dd988dc9548cd12
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-14 11:45:24 -0400

    looping through array of objects

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-//Array inside an obj
+// //Array inside an obj
 
-const adventurer = {
-  name: "Ernest",
-  hitPoint: 10,
-  belongings: ["sword", "potions", "medicine"],
-  companion: {
-    name: "K.C.",
-    type: "Dog",
-  },
-};
+// const adventurer = {
+//   name: "Ernest",
+//   hitPoint: 10,
+//   belongings: ["sword", "potions", "medicine"],
+//   companion: {
+//     name: "K.C.",
+//     type: "Dog",
+//     belongings: ["toy ball", "healing potions", "cash", "treats"],
+//   },
+// };
 
-console.log(adventurer.companion.name);
+// console.log(`My companion's name is ${adventurer.companion.name}`);
+// console.log(
+//   `One of my companion's belongings is ${adventurer.companion.belongings[2]}`);
 // //Access values in the belongings array
 // console.log(adventurer.belongings);
 
@@ -24,3 +27,21 @@ console.log(adventurer.companion.name);
 // }
 
 // #1 Obj within Obj
+
+const movies = [
+  {
+    title: "Toy Story",
+  },
+  {
+    title: "Lion King",
+  },
+  {
+    title: "Adventure",
+  },
+];
+
+console.log(movies[0].title);
+// Looping over array of objects
+for (let i = 0; i < movies.length; i++) {
+  console.log(movies[i]).title;
+}
